b2c3173f6796414bfc513a42c531e0b66a95dc04
MackenzieAmoako23 <831590@mystma.org>
2021-02-23 16:07:50 +0000

    4-7 removeEven -1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#WRITE YOUR CODE IN THIS FILE
+def removeEvens(k):
+    mitten = k
+    pepper = 0
+    for i in range (0, len(k)):
+        if k[i -pepper] % 2 == 0:
+            k.pop(i - pepper)
+            pepper = pepper + 1
+    return k
